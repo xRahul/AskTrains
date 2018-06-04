ebd8abb7c080852d0b6a80140d8612ee0c065c0b
Rahul Jain <rahul_jain@live.in>
2018-06-04 10:46:47 +0530

    Welcome and fallback intents improved

--- src/intents/fallback.js
+++ src/intents/fallback.js
@@ -1,6 +1,10 @@
+import { Suggestion } from 'dialogflow-fulfillment'
+
+
 function fallback(agent) {
-  agent.add('I didn\'t understand')
-  agent.add('I\'m sorry, can you try again?')
+  agent.add('Sorry, I didn\'t get that. You can ask me for trains running between stations or ask for the live status of a train.')
+  agent.add(new Suggestion('Show trains from JP to BDTS for tomorrow.'))
+  agent.add(new Suggestion('What is the live status of 13008 for yesterday?'))
 }
 
 export default fallback
--- src/intents/welcome.js
+++ src/intents/welcome.js
@@ -1,5 +1,10 @@
+import { Suggestion } from 'dialogflow-fulfillment'
+
+
 function welcome(agent) {
-  agent.add('Welcome to my agent!')
+  agent.add('Hi there. You can ask me for trains running between stations or ask for the live status of a train.')
+  agent.add(new Suggestion('Show trains from JP to BDTS for tomorrow.'))
+  agent.add(new Suggestion('What is the live status of 13008 for yesterday?'))
 }
 
 export default welcome
