1bd1dbbc0e7f7881c41c7aac9a8cb1f5e4167497
Rahul Jain <rahul_jain@live.in>
2018-07-01 21:32:32 +0530

    Suggestion Chips are made <25 Characters.

--- src/intents/fallback.js
+++ src/intents/fallback.js
@@ -3,8 +3,8 @@ import { Suggestion } from 'dialogflow-fulfillment'
 
 function fallback(agent) {
   agent.add('Sorry, I didn\'t get that. You can ask me for trains running between stations or ask for the live status of a train.')
-  agent.add(new Suggestion('Show trains from JP to BDTS for tomorrow.'))
-  agent.add(new Suggestion('What is the live status of 13008 for yesterday?'))
+  agent.add(new Suggestion('Trains from JP to BDTS'))
+  agent.add(new Suggestion('Live status of 13008'))
 }
 
 export default fallback
--- src/intents/welcome.js
+++ src/intents/welcome.js
@@ -3,8 +3,8 @@ import { Suggestion } from 'dialogflow-fulfillment'
 
 function welcome(agent) {
   agent.add('Hi there. You can ask me for trains running between stations or ask for the live status of a train.')
-  agent.add(new Suggestion('Show trains from JP to BDTS for tomorrow.'))
-  agent.add(new Suggestion('What is the live status of 13008 for yesterday?'))
+  agent.add(new Suggestion('Trains from JP to BDTS'))
+  agent.add(new Suggestion('Live status of 13008'))
 }
 
 export default welcome
